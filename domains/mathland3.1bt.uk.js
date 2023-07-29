eec5bd42f772fe98c7001d754cdaf25567a5907a
0Midnite <104600774+0Midnite@users.noreply.github.com>
2023-07-29 17:36:41 -0400

    Create mathland3.1bt.uk.js

---
+++ domains/mathland3.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'unblocked games site',
+  domain: '1bt.uk',
+  subdomain: 'mathland3',
+  owner: {
+    email: '0midnite@0midnite.com',
+    // dont email me pls thx
+  },
+  record: {
+    CNAME: '381f42a0-2297-4aa4-956d-954a8b89e660.id.repl.co',
+  },
+})
